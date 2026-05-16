928a7d95cdae65e8976881bb274586a6e16da530
Ashutosh Bhonsle <bhonsle.ashutosh@gmail.com>
2026-05-16 23:56:45 +0530

    Window pops up

--- main.py
+++ main.py
@@ -10,11 +10,26 @@ pygame.init()
 pygame.display.set_caption("Fencing")
 
 BG_COLOR=(255,255,255)
-WIDTH,HEIGTH=1000,800
+WIDTH,HEIGHT = 1000,500
 FPS=60
 PLAYER_VEL = 5
 
-window = pygame.display.set_mode(WIDTH,HEIGTH)
+window = pygame.display.set_mode((WIDTH,HEIGHT))
 
-def main():
-    
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
