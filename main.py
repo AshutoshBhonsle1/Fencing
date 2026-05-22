703ff9a07cdca4876c3175bcfeb7c0502e8a0924
Ashutosh Bhonsle <bhonsle.ashutosh@gmail.com>
2026-05-23 00:06:49 +0530

    Added move scripts

--- main.py
+++ main.py
@@ -15,6 +15,34 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+class Player(pygame.sprite.Sprite()): #inherts from pygame.sprite
+
+    COLOR = (255,0,0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x,y,width,height) #to get pixel perfect collisions(i think)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = 0
+        self.direction="left"
+        self.animation_count=0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction!="left":
+            self.direction="left"
+            self.animation_count=0
+
+    def move_right(self, vel):
+        self.x_vel= +vel
+        if self.direction!="right":
+            self.direction="right"
+            self.animation_count=0
+
 
 def get_background(name):
     image = pygame.image.load(join("assets","Background",name))
